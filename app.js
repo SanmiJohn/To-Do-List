e6ead7b0996438f6b1b20cca09811fc36f46c09a
Oluwasanmi John <105114113+SanmiJohn@users.noreply.github.com>
2023-02-11 20:52:52 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,39 @@
 const TodoBox = document.getElementById('todobox')
 const Addbtn = document.querySelector(".addbtn")
-const Input = document.querySelector("#input")
+const Inputbox = document.querySelector("#input")
 
 function Add() {
-    console.log(Input.value)
-    Input.value = ''
-    TodoBox.classList.add('todoitems')
+    if (Inputbox.value) {
+        let TodoBar = document.createElement('div')
+        let TodoWords = document.createElement('span')
+        TodoWords.classList.add('todowords')
+        TodoBar.classList.add('todoitems')
 
-    let InputValue = document.createElement('div')
-    InputValue.innerText = Input.value
-    TodoBox.append(InputValue)
+        let Hr = document.createElement('hr')
+        Hr.classList.add('hrline')
+
+        let Icon = document.createElement('i')
+        Icon.classList.add('fa-solid', 'fa-trash')
+
+        TodoWords.innerText = Inputbox.value
+
+        TodoBar.append(TodoWords)
+        TodoBar.append(Icon)
+        TodoBar.append(Hr)
+        TodoBox.append(TodoBar)
+
+        Inputbox.value = ''
+    } else {
+        alert('Input task to be done')
+    }
+
+    function Cross() {
+        Span.classList.add('crossitem')
+    }
+
+    let Span = document.querySelector('span')
+    Span.addEventListener("click", Cross)
 }
 
 Addbtn.addEventListener("click", Add)
-// Addbtn.addEventListener("click", Cross)
 

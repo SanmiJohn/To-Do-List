523a205c92c63f121a1846c5dc615779c6477998
Oluwasanmi John <105114113+SanmiJohn@users.noreply.github.com>
2023-02-13 19:55:17 +0100

    Update app.js
    Found how to use element.remove

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const TodoBox = document.getElementById('todobox')
 const Addbtn = document.querySelector(".addbtn")
 const Inputbox = document.querySelector("#input")
 
+// let TodoBar = document.createElement('div')
+// let TodoWords = document.createElement('span')
+// let Hr = document.createElement('hr')
+// let Icon = document.createElement('i')
+
 function Add() {
     if (Inputbox.value) {
         let TodoBar = document.createElement('div')
@@ -33,7 +38,31 @@ function Add() {
 
     let Span = document.querySelector('span')
     Span.addEventListener("click", Cross)
+
+
+    function Delete() {
+        let TodoBar = document.createElement('div')
+        let TodoWords = document.createElement('span')
+        let Hr = document.createElement('hr')
+        let Icon = document.createElement('i')
+
+        console.log('Glory to GOD')
+
+        TodoWords.classList.remove('todowords')
+        TodoBar.classList.remove('todoitems')
+        Hr.classList.remove('hrline')
+        Icon.classList.remove('fa-solid', 'fa-trash')
+
+        // TodoBar.remove(TodoWords)
+        // TodoBar.remove(Icon)
+        // TodoBar.remove(Hr)
+        // TodoBox.remove(TodoBar)
+    }
+
+    let Delbtn = document.querySelector('i')
+    Delbtn.addEventListener("click", Delete)
 }
 
 Addbtn.addEventListener("click", Add)
+
 
